d6e4b39cdea330c0b6cd22a0cf04fca32c0a73a4
ㄱㄱㅇ <77200026+jjiayan@users.noreply.github.com>
2026-04-20 14:11:06 +0900

    [level 2] Title: 기능개발, Time: 0.06 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,21 +1,17 @@
 import math
-import collections
 
 def solution(progresses, speeds):
-    que = collections.deque()
+    max_day = 0
     res = []
     
     for i in range(len(progresses)):
         # 개발은 미리 할 수 있고, 배포는 순서대로
         dev = math.ceil((100 - progresses[i]) / speeds[i]) 
-        if i == 0 or que[-1] < dev:
-            que.append(dev)
+        if i == 0 or max_day < dev:
+            max_day = dev
             res.append(1)
-        elif que[-1] >= dev:
+        elif max_day >= dev:
             res[-1] += 1
     
     return res
-            
-            
-        
-    return list(que)
+
