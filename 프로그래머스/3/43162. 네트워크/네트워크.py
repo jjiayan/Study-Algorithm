84b02d3fddccdc1e8cfd84b51ba5dc167ab39182
ㄱㄱㅇ <77200026+jjiayan@users.noreply.github.com>
2026-06-08 11:42:00 +0900

    [level 3] Title: 네트워크, Time: 0.39 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,21 +1,25 @@
+
+
 def solution(n, computers):
     visited = [False] * n
     network_cnt = 0
     stack = []
     
+    def dfs(start):
+        stack = [start]
+        while stack:
+            node = stack.pop()
+
+            if not visited[node]:
+                visited[node] = True
+
+                for j in range(n):
+                    if j != node and computers[node][j] == 1:
+                        stack.append(j)
+    
     for i in range(n):
         if not visited[i]:
-            stack.append(i)
-        
-            while stack:
-                node = stack.pop()
-
-                if not visited[node]:
-                    visited[node] = True
-
-                    for j in range(n):
-                        if j != node and computers[node][j] == 1:
-                            stack.append(j)
+            dfs(i)
             network_cnt += 1
             
     return network_cnt
